049551cecc4c76b057ce1740b5443282aff5540b
hoffmann michel <michel.hof@hotmail.fr>
2023-09-01 12:45:56 +0200

    Mise en place MVC pour les filtres

---
+++ .history/API/cars_20230901102320.php
@@ -0,0 +1,118 @@
+<?php
+class Model{
+
+    private $dbh;
+    
+    public function __construct()
+    {
+        $dsn = 'mysql:host=localhost;dbname=garage;charset=utf8';
+        $user = 'root';
+        $password = '';
+        
+        try {
+            // Connexion à la base de données
+            $this->dbh = new PDO($dsn, $user, $password);
+        } catch (PDOException $e) {
+            die('Erreur de connexion : ' . $e->getMessage());
+        }
+        
+    }
+    
+    public function getCarsByFilters($filters) {
+        // Construct the SQL query based on filters
+        $sql = "SELECT * FROM vehicule WHERE 1";
+    
+        if (isset($filters['marque'])) {
+            $sql .= " AND marque = :marque";
+        }
+    
+        if (isset($filters['kilometremin'])) {
+            $sql .= " AND kilometrage >= :kilometremin";
+        }
+    
+        if (isset($filters['kilometremax'])) {
+            $sql .= " AND kilometrage <= :kilometremax";
+        }
+    
+        if (isset($filters['anneemin'])) {
+            $sql .= " AND annee >= :anneemin";
+        }
+        if (isset($filters['anneemax'])) {
+            $sql .= " AND annee <= :anneemax";
+        }
+    
+        if (isset($filters['prixmin'])) {
+            $sql .= " AND prix >= :prixmin";
+        }
+    
+        if (isset($filters['prixmax'])) {
+            $sql .= " AND prix <= :prixmax";
+        }
+        // Prepare and execute the query
+        $stmt = $this->dbh->prepare($sql);
+        $stmt->execute($filters);
+    
+        // Fetch results
+        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    
+        return $results;
+    }
+     }
+    
+
+// Création du controlleur du côté front qui va regrouper toutes nos routes
+class Controller{
+    //Crérera automatiquement une instance de classe APIManager et la stockera ds la propriété $apiManager. Cela sera utile pour gérer le JSON par la suite.
+    private $apiManager;//propriété privée (accessible uniquement à l'intérieur de cette classe) qui va contenir une instance de la classe APIManager.
+
+    public function __construct(){
+        $this->apiManager = new Model();
+    }
+
+public function getCarsByFilters($filtres){
+    $cars=$this->apiManager->getCarsByFilters($filtres);
+    header('Content-Type: application/json');
+        echo json_encode($cars);
+}
+}
+
+$filters = ["marque"=>"citroen", "kilometremin"=>0, "kilometremax"=>200000, "anneemin"=>2000,"anneemax"=>2023,"prixmin"=>0,"prixmax"=>50000  ];
+
+$marque = $_GET['marque'];
+$kilometrmin = $_GET['kilometragemin'];
+$year = $_GET['year'];
+$price = $_GET['price'];
+if (isset($filters['marque'])) {
+    $sql .= " AND marque = :marque";
+}
+
+if (isset($filters['kilometremin'])) {
+    $sql .= " AND kilometrage >= :kilometremin";
+}
+
+if (isset($filters['kilometremax'])) {
+    $sql .= " AND kilometrage <= :kilometremax";
+}
+
+if (isset($filters['anneemin'])) {
+    $sql .= " AND annee >= :anneemin";
+}
+if (isset($filters['anneemax'])) {
+    $sql .= " AND annee <= :anneemax";
+}
+
+if (isset($filters['prixmin'])) {
+    $sql .= " AND prix >= :prixmin";
+}
+
+if (isset($filters['prixmax'])) {
+    $sql .= " AND prix <= :prixmax";
+}
+   
+$controller = new Controller();
+
+$controller->getCarsByFilters($filters);
+
+
+
+?>
---
+++ .history/API/cars_20230901103921.php
@@ -0,0 +1,122 @@
+<?php
+class Model{
+
+    private $dbh;
+    
+    public function __construct()
+    {
+        $dsn = 'mysql:host=localhost;dbname=garage;charset=utf8';
+        $user = 'root';
+        $password = '';
+        
+        try {
+            // Connexion à la base de données
+            $this->dbh = new PDO($dsn, $user, $password);
+        } catch (PDOException $e) {
+            die('Erreur de connexion : ' . $e->getMessage());
+        }
+        
+    }
+    
+    public function getCarsByFilters($filters) {
+        // Construct the SQL query based on filters
+        $sql = "SELECT * FROM vehicule WHERE 1";
+    
+        if (isset($filters['marque'])) {
+            $sql .= " AND marque = :marque";
+        }
+    
+        if (isset($filters['kilometremin'])) {
+            $sql .= " AND kilometrage >= :kilometremin";
+        }
+    
+        if (isset($filters['kilometremax'])) {
+            $sql .= " AND kilometrage <= :kilometremax";
+        }
+    
+        if (isset($filters['anneemin'])) {
+            $sql .= " AND annee >= :anneemin";
+        }
+        if (isset($filters['anneemax'])) {
+            $sql .= " AND annee <= :anneemax";
+        }
+    
+        if (isset($filters['prixmin'])) {
+            $sql .= " AND prix >= :prixmin";
+        }
+    
+        if (isset($filters['prixmax'])) {
+            $sql .= " AND prix <= :prixmax";
+        }
+        // Prepare and execute the query
+        $stmt = $this->dbh->prepare($sql);
+        $stmt->execute($filters);
+    
+        // Fetch results
+        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    
+        return $results;
+    }
+     }
+    
+
+// Création du controlleur du côté front qui va regrouper toutes nos routes
+class Controller{
+    //Crérera automatiquement une instance de classe APIManager et la stockera ds la propriété $apiManager. Cela sera utile pour gérer le JSON par la suite.
+    private $apiManager;//propriété privée (accessible uniquement à l'intérieur de cette classe) qui va contenir une instance de la classe APIManager.
+
+    public function __construct(){
+        $this->apiManager = new Model();
+    }
+
+public function getCarsByFilters($filtres){
+    $cars=$this->apiManager->getCarsByFilters($filtres);
+    header('Content-Type: application/json');
+        echo json_encode($cars);
+}
+}
+
+
+$marque = $_GET['marque'];
+$kilometremin = $_GET['kilometremin'];
+$kilometremax = $_GET['kilometremax'];
+$anneemin = $_GET['anneemin'];
+$anneemax = $_GET['anneemax'];
+$prixmin = $_GET['prixmin'];
+$prixmax = $_GET['prixmax'];
+
+$filters = array();
+if (isset($marque)) {
+    $filters["marque"] = $marque;
+}
+
+if (isset($kilometremin)) {
+    $filters['kilometremin'] = $kilometremin;
+}
+
+if (isset($kilometremax)) {
+    $filters['kilometremax'] = $kilometremax;
+}
+
+if (isset()) {
+    $sql .= " AND annee >= :anneemin";
+}
+if (isset($filters['anneemax'])) {
+    $sql .= " AND annee <= :anneemax";
+}
+
+if (isset($filters['prixmin'])) {
+    $sql .= " AND prix >= :prixmin";
+}
+
+if (isset($filters['prixmax'])) {
+    $sql .= " AND prix <= :prixmax";
+}
+   
+$controller = new Controller();
+
+$controller->getCarsByFilters($filters);
+
+
+
+?>
