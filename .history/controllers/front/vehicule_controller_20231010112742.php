48f1d82c813f339548619b896cd060ae577532e3
HOFFMANN MICHEL <michel.hof@hotmail.fr>
2023-10-10 12:38:47 +0200

    filtres

--- .history/controllers/front/vehicule_controller_20231010112742.php
+++ .history/controllers/front/vehicule_controller_20231010112742.php
@@ -20,15 +20,12 @@ class VehiculeController
 
        
         $cars = $this->apiManager->getCarsByFilters($filtres);
-       
+        echo json_encode($cars);
 
           //Configurez les entêtes avant d'envoyer la réponse
-           header('Content-Type: application/json');
+         header('Content-Type: application/json');
            header("Access-Control-Allow-Origin: http://localhost:3000");
-           header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-           header("Access-Control-Allow-Headers: Content-Type");
-          
-           echo json_encode($cars);
-        }
-    
+        //   header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+        //   header("Access-Control-Allow-Headers: Content-Type");
+    }
 }
--- .history/controllers/front/vehicule_controller_20231010112823.php
+++ .history/controllers/front/vehicule_controller_20231010112823.php
@@ -23,7 +23,7 @@ class VehiculeController
        
 
           //Configurez les entêtes avant d'envoyer la réponse
-           header('Content-Type: application/json');
+         header('Content-Type: application/json');
            header("Access-Control-Allow-Origin: http://localhost:3000");
            header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
            header("Access-Control-Allow-Headers: Content-Type");
